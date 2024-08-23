58235b17b25585033c6a589e19cc5d7f7486c89c
Walmy20 <74880546+Walmy20@users.noreply.github.com>
2024-08-23 15:31:43 -0400

    Update print_pdfs.bat

--- print_pdfs.bat
+++ print_pdfs.bat
@@ -14,9 +14,8 @@ set "acrobat_path=C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
  
 for /r "%pdf_directory%" %%F in (*.pdf) do (
     echo Printing: %%~nF
-    start "" /wait "%acrobat_path%" /n /s /o /h /t "%%F"
+    start "" "%acrobat_path%" /n /s /o /h /t "%%F"
     timeout /t 5 /nobreak > nul  :: Add a 5-second delay between prints to ensure the document is sent to the printer
-    taskkill /IM acrobat.exe /F  :: Forcefully close Adobe Acrobat
 )
  
 endlocal
